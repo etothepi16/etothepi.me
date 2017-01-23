e390e4c760bf12c29ec0d1426aeceab9499d2e36
etothepi16 <paulmcdowell18@gmail.com>
2017-01-22 23:16:08 -0500

    Update sketch.js

--- mandelbrot/sketch.js
+++ mandelbrot/sketch.js
@@ -12,10 +12,10 @@ function setup() {
 
   minSlider = createSlider(-2.5, 0, -2.5, 0.01);
   minSlider.parent('sketch-holder');
-  minSlider.position(250,250);
+  minSlider.position(250,300);
   maxSlider = createSlider(0, 2.5, 2.5, 0.01);
   maxSlider.parent('sketch-holder');
-  maxSlider.position(250,280);
+  maxSlider.position(250,330);
 }
 
 function draw() {
